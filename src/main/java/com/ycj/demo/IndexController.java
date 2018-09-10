028c175dac1cbd6221c8d08a3bee10851c04d48f
ycj <532446306@qq.com>
2018-09-10 18:09:42 +0800

    init

---
+++ src/main/java/com/ycj/demo/IndexController.java
@@ -0,0 +1,19 @@
+package com.ycj.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author JackSpeed
+ * @version V1.0 <>
+ * @date 2018-09-10 18:04
+ */
+@RestController
+public class IndexController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
+        return "docker Jenkins 测试";
+    }
+}
